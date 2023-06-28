768e9c34b9115221708798f8a8e5a7f12ec78b64
Vitor Tognetti Baradelli <vitorbaradelli@gmail.com>
2023-06-28 12:08:26 -0300

    :rocket:

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./styles/global.css";
+
+import { Provider as ReduxProvider } from "react-redux";
+
+import { store } from "./store";
+import { Player } from "./pages/Player";
+
+function App() {
+  return (
+    <div>
+      <ReduxProvider store={store}>
+        <Player />
+      </ReduxProvider>
+    </div>
+  );
+}
+
+export default App;
